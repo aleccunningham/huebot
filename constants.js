d9ae51ce8d2d8759c50c34fdc5721e3ffe8d80f6
aleccunningham <aleccunningham96@gmail.com>
2017-09-25 17:48:15 -0500

    fixed kitchen constants

--- constants.js
+++ constants.js
@@ -3,9 +3,9 @@
     LIVING_ROOM_ON: '/livingroom_on';
     LIVING_ROOM_DIM: '/livingroom_dim';
     LIVING_ROOM_MOVIE: '/livingroom_movie';
-    KITCHEN_ROOM_OFF: '/kitchen_room_off';
-    KITCHEN_ROOM_ON: '/kitchen_room_on';
-    KITCHEN_ROOM_DIM: '/kitchen_room_dim';
+    KITCHEN_ROOM_OFF: '/kitchen_off';
+    KITCHEN_ROOM_ON: '/kitchen_on';
+    KITCHEN_ROOM_DIM: '/kitchen_dim';
     BEDROOM_ROOM_OFF: '/bedroom_off';
     BEDROOM_ROOM_ON: '/bedroom_on';
     BEDROOM_ROOM_DIM: '/bedroom_dim';
