225da7e5fbdcd25a3a18a5bdd8f1f8a892bf7e2a
aleccunningham <aleccunningham96@gmail.com>
2017-09-25 17:08:11 -0500

    switch statement for applets

--- bot.js
+++ bot.js
@@ -1,15 +1,89 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var events = require('./constants');
 
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+  var request = JSON.parse(this.req.chunks[0])
+
+  switch(request) {
+    case events.LIVING_ROOM_ON:
+      data = {
+        "event": "living_room_on",
+      }
+      break;
+    case events.LIVING_ROOM_OFF:
+      data = {
+        "event": "living_room_off",
+      }
+      break;
+    case events.LIVING_ROOM_DIM:
+      data = {
+        "event": "living_room_dim",
+        "Value1": dimValue
+      }
+      break;
+    case events.LIVING_ROOM_MOVIE:
+      data = {
+        "event": "living_room_movie",
+      }
+    case events.SUNROOM_ROOM_ON:
+      data = {
+        "event": "sunroom_room_on",
+      }
+      break;
+    case events.SUNROOM_ROOM_OFF:
+      data = {
+        "event": "sunroom_room_off",
+      }
+      break;
+    case events.SUNROOM_ROOM_DIM:
+      data = {
+        "event": "sunroom_room_dim",
+        "Value1": dimValue
+      }
+      break;
+    case events.KITCHEN_ROOM_ON:
+      data = {
+        "event": "kitchen_room_on",
+      }
+      break;
+    case events.KITCHEN_ROOM_OFF:
+      data = {
+        "event": "kitchen_room_off",
+      }
+      break;
+    case events.KITCHEN_ROOM_DIM:
+      data = {
+        "event": "kitchen_room_dim",
+        "Value1": dimValue
+      }
+      break;
+    case events.BEDROOM_ROOM_ON:
+      data = {
+        "event": "bedroom_room_on",
+      }
+      break;
+    case events.BEDROOM_ROOM_OFF:
+      data = {
+        "event": "bedroom_room_off",
+      }
+      break;
+    case events.BEDROOM_ROOM_DIM:
+      data = {
+        "event": "bedroom_room_dim",
+        "Value1": dimValue
+      }
+      break;
+    default:
+      data = {
+        "event": "error",
+      }
+  }
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(data);
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,20 +92,30 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(data) {
   var botResponse, options, body, botReq;
-
-  botResponse = cool();
 
   options = {
     hostname: 'maker.ifttt.com',
-    path: `/trigger/{event}/with/key/defFRlgcG0s8F0w53vR_kF`,
+    path: `/trigger/${event}/with/key/defFRlgcG0s8F0w53vR_kF`,
     method: 'POST'
   };
 
   body = {
-    "event" : hueCmd,sa
+    "event": data["event"],
+    "Value1": data["Value1"],
   };
+
+  groupme = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  groupme_body = {
+    "bot_id": botID,
+    "text": data["event"]
+  }
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
@@ -50,6 +134,17 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
+
+  groupmeReq = HTTPS.request(groupme, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  groupmeReq.end(JSON.stringify(groupme_body));
+
 }
 
 
